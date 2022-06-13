2bbab2db534711cce97a7264275c6e09440a19b1
Darkpaster <egorokkk5000@mail.ru>
2022-06-13 22:10:49 +0300

    Created beginning of the game, freezes

--- bd.php
+++ bd.php
@@ -1,7 +1,7 @@
 <?php 
-//$connection = mysqli_connect('localhost', 'id17956435_lightpaster', '@%mW-OoeI#idzv2X', 'id17956435_accounts');
+$connection = mysqli_connect('localhost', 'id17956435_lightpaster', '@%mW-OoeI#idzv2X', 'id17956435_accounts');
 
-$connection = mysqli_connect('localhost', 'root', '', 'accounts');
+//$connection = mysqli_connect('localhost', 'root', '', 'accounts');
 
 // if ($connection) {
 // 	echo 'bd says hello';
